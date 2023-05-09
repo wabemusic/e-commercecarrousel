a4c665d69ef89b65a6e997fa8dc22782e4b0186c
João Vicente Watanabe <100879173+wabemusic@users.noreply.github.com>
2023-05-08 21:07:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const caixaImagem = document.querySelectorAll(".caixa-img");
+
+caixaImagem.forEach(function (caixa) {
+  const imagens = caixa.querySelectorAll("img");
+  let idx = 0;
+
+  function carrosel() {
+    idx++;
+
+    if (idx > imagens.length - 1) {
+      idx = 0;
+    }
+
+    caixa.style.transform = `translateX(${-idx * 310}px)`;
+  }
+
+  setInterval(carrosel, 2500);
+});
+
+//encerrou o slider
+
+let botoes = document.querySelectorAll('.colors div')
+
+botoes.forEach(function(botao){
+    botao.addEventListener('click', function(){
+      botoes.forEach(function(outroBotao){
+        outroBotao.classList.remove('selecionado')
+    })
+
+    botao.classList.add('selecionado')
+  })
+})
